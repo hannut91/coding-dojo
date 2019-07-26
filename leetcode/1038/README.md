54388aafc792a699a2d5efbe59e837c20ca34dc4
hannut91 <hannut91@gmail.com>
2019-07-26 23:22:03 +0900

    Add Binary Search Tree to Greater Sum Tree
    Add Binary Search Tree to Greater Sum Tree solution.
    
    See also
      - https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree

---
+++ leetcode/1038/README.md
@@ -0,0 +1,16 @@
+# 1038. Binary Search Tree to Greater Sum Tree
+
+## 1. 이해
+
+Binary root가 주어졌을 때 각 노드를 포함하여 더 큰 값을 가지고 있는 노도들의
+합으로 만들어진 새로운 Binary tree를 만들어라.
+
+## 2. 계획
+
+## 3. 실행
+
+## 4. 회고
+
+## Sources
+
+- <https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree>
---
+++ leetcode/1038/binary-search-tree-to-greater-sum-tree.test.js
@@ -0,0 +1,121 @@
+function TreeNode(val) {
+  this.val = val;
+  this.left = null;
+  this.right = null;
+
+  this.insert = value => {
+    if (!this.val) {
+      this.val = value;
+      return;
+    }
+
+    if (value < this.val) {
+      if (!this.left) {
+        this.left = new TreeNode(value);
+        return;
+      }
+
+      this.left.insert(value);
+      return;
+    }
+
+    if (value > this.val) {
+      if (!this.right) {
+        this.right = new TreeNode(value);
+        return;
+      }
+
+      this.right.insert(value);
+    }
+  };
+
+  this.reverseInOrder = callback => {
+  };
+
+  this.toArray = () => {
+    const queue = [];
+    const output = [];
+
+    queue.push(this);
+
+    while (queue.length > 0) {
+      const node = queue.shift();
+      output.push(node.val);
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+
+    return output;
+  };
+}
+
+const createBinaryTree = values => {
+  const root = new TreeNode(values.shift());
+
+  const queue = [root];
+
+  while (values.length > 0) {
+    const node = queue.shift();
+
+    node.left = new TreeNode(values.shift());
+    node.right = new TreeNode(values.shift());
+
+    queue.push(node.left);
+    queue.push(node.right);
+  }
+
+  return root;
+};
+
+const reverseInOrder = (node, callback) => {
+  if (node.right && node.right.val !== null) {
+    reverseInOrder(node.right, callback);
+  }
+
+  callback(node);
+
+  if (node.left && node.left.val !== null) {
+    reverseInOrder(node.left, callback);
+  }
+};
+
+const bstToGst = root => {
+  let sum = 0;
+
+  reverseInOrder(root, node => {
+    node.val += sum;
+    sum = node.val;
+  });
+
+  return root;
+};
+
+test('create binary tree', () => {
+  const root = createBinaryTree([
+    4, 1, 6, 0, 2, 5, 7, null, null, null, 3, null, null, null, 8,
+  ]);
+
+  expect(root.toArray()).toEqual([
+    4, 1, 6, 0, 2, 5, 7, null, null, null, 3, null, null, null, 8,
+  ]);
+});
+
+test('bstToGst', () => {
+  const root = bstToGst(
+    createBinaryTree([
+      4, 1, 6, 0, 2, 5, 7, null, null, null, 3, null, null, null, 8,
+    ]),
+  );
+
+  console.log(root.toArray());
+
+  expect(root.toArray()).toEqual([
+    30, 36, 21, 36, 35, 26, 15, null, null, null, 33, null, null, null, 8,
+  ]);
+});
