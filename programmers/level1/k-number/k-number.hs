aa64ca7d50f05f7982f1b8da06e57bc1810a4bb5
hannut91 <hannut91@gmail.com>
2020-03-15 18:16:01 +0900

    Add solution using Monad

--- programmers/level1/k-number/k-number.hs
+++ programmers/level1/k-number/k-number.hs
@@ -2,13 +2,37 @@ import Test.Hspec
 import Control.Exception (evaluate)
 import Data.List
 
+-- Using Monad
 kNumber :: [Int] -> [[Int]] -> [Int]
-kNumber a c = map m c
-  where m (f:t:k:[]) = (sort $ slice (f - 1) t a) !! (k - 1)
+kNumber a c = do
+  (f:t:k:[]) <- c
+  x <- [slice (f - 1) t a]
+  y <- [sort x]
+  z <- [y !! (k - 1)]
+  return z
 
 slice :: Int -> Int -> [Int] -> [Int]
-slice _ _ [] = []
-slice from to arr = take (to - from) $ drop from arr
+slice from to arr = do
+  x <- [drop from arr]
+  y <- take (to - from) x
+  return y
+
+-- Normal way
+-- kNumber :: [Int] -> [[Int]] -> [Int]
+-- kNumber a c = map m c
+--   where m (f:t:k:[]) = (sort $ slice (f - 1) t a) !! (k - 1)
+
+-- slice :: Int -> Int -> [Int] -> [Int]
+-- slice _ _ [] = []
+-- slice from to arr = take (to - from) $ drop from arr
+
+-- Using Applicative Functor
+-- kNumber :: [Int] -> [[Int]] -> [Int]
+-- kNumber a c = map m c
+--   where m (f:t:k:[]) = (sort $ slice (f - 1) t a) !! (k - 1)
+
+-- slice from to arr = take <$> Just (to - from)
+--   <*> (drop <$> Just from <*> Just arr)
 
 main = hspec $ do
   describe "kNumber" $ do
